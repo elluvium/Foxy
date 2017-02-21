d8d6c9a54dd8a0f078ebe7c30b957355e445fd9c
Vadim <vadimpenchak@gmail.com>
2017-02-21 12:19:20 +0200

    Added static method "ExponentiateMatrix" to "Logic" (not tested at all) with comments. Added comments to FoxMath.

--- Logic/FoxMath.cs
+++ Logic/FoxMath.cs
@@ -7,8 +7,22 @@ using System.Threading.Tasks;
 
 namespace Logic
 {
+    /// <summary>
+    /// Contains static methods to be used to calculate all math in lab tasks.
+    /// </summary>
     public static class FoxMath
     {
-        
+        /// <summary>
+        /// Multiplies given matrix by itself one time.
+        /// </summary>
+        /// <param name="matrix">Square matrix of Int32's</param>
+        /// <returns>(input matrix) ^ 2</returns>
+        public static int[,] ExponentiateMatrix(int[,] matrix) // TODO Add data-driven unit-tests
+        {
+            var temp = Matrix<int>.Build.DenseOfArray(matrix);
+            temp.Multiply(temp);
+
+            return temp.ToArray();
+        }
     }
 }
