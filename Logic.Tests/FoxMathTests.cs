cb8b9f02e6e1033882eae26e7f82d22df37c841a
Iaroslav_Rudchenko <Iaroslav_Rudchenko@epam.com>
2017-02-23 22:30:07 +0300

    [Implement Unit Test for Exponential method] - add another implementation

--- Logic.Tests/FoxMathTests.cs
+++ Logic.Tests/FoxMathTests.cs
@@ -13,6 +13,7 @@ namespace Logic.Tests
     public class FoxMathTests
     {
         // TODO Implement data-driven unit test for ExponentiateMatrix
+        //i can't understand what test should do exactly(boolship), below i give you my variant of implementation 
         [TestMethod()]
         public void ExponentiateMatrixTest()
         {
@@ -24,6 +25,32 @@ namespace Logic.Tests
                 actualMatrix.Cast<double>().SequenceEqual(expectedMatrix.Cast<double>());
 
             Assert.AreEqual(equal, true);
+        }
+
+        [TestMethod()]
+        public void ExponentiateMatrix_ReturnMultiplicateMatrix_IfInitDataValid()
+        {
+            double[,] initialMatrix = 
+            {
+                { 1, 1, 1 },
+                { 0, 0, 1 },
+                { 0, 0, 1 }
+            };
+
+            double[,] expectedMatrix = 
+            { 
+                { 1, 1, 3 },
+                { 0, 0, 1 },
+                { 0, 0, 1 }
+            };
+
+            var matrix = FoxMath.ExponentiateMatrix(initialMatrix);
+            var matrixToArray = matrix.Cast<double>().ToArray();
+            var expectedMatrixToArray = expectedMatrix.Cast<double>().ToArray();
+
+            var isEquals = matrixToArray.SequenceEqual(expectedMatrixToArray);
+
+            Assert.IsTrue(isEquals);
         }
     }
 }
