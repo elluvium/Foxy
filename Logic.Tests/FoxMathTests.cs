b6091eb7bbd8da438380ed4a695f9b6c538c6319
Stanislav Kostenko <kostenkostas@gmail.com>
2017-02-23 17:33:48 +0200

    Performed Unit Testing

--- Logic.Tests/FoxMathTests.cs
+++ Logic.Tests/FoxMathTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Logic;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,14 @@ namespace Logic.Tests
         [TestMethod()]
         public void ExponentiateMatrixTest()
         {
-            Assert.Fail();
+            double[,] matrix = { {1, 1, 1}, {0, 0, 1}, {0, 0, 1} };
+            double[,] expectedMatrix = { { 1, 1, 3 }, { 0, 0, 1 }, { 0, 0, 1 } };
+            
+            var actualMatrix = FoxMath.ExponentiateMatrix(matrix);
+            bool equal = actualMatrix.Rank == expectedMatrix.Rank && Enumerable.Range(0, actualMatrix.Rank).All(dimension => actualMatrix.GetLength(dimension) == expectedMatrix.GetLength(dimension)) && 
+                actualMatrix.Cast<double>().SequenceEqual(expectedMatrix.Cast<double>());
+
+            Assert.AreEqual(equal, true);
         }
     }
 }
