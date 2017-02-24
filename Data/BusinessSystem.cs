da5a53a0f31873fbebf8df86b06240cbcc7b319a
Vadim <vadimpenchak@gmail.com>
2017-02-24 15:10:02 +0200

    Added classes to contain business info and data.

---
+++ Data/BusinessSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Data
+{
+    public class BusinessSystem
+    {
+        public string Title { get; set; }
+        public string Vision { get; set; }
+        public string Mission { get; set; }
+        public string GlobalGoal { get; set; }
+
+        public List<string> FunctionalZones { get; set; }
+        public List<string> KeyAreas { get; set; }
+
+        public List<Goal> Goals { get; set; }
+    }
+}
---
+++ Data/Goal.cs
@@ -0,0 +1,9 @@
+﻿namespace Data
+{
+    public class Goal
+    {
+        public uint Index { get; set; }
+        public string Content { get; set; }
+        public string ProvidesFor { get; set; } // TODO edit get() to return int[]
+    }
+}
