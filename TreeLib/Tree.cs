499095605af2250e14f627d0c72307c9196d8678
Demius <ahernar777@gmail.com>
2017-03-01 21:13:54 +0200

    TreeLib Project Implementation of Tree data structure (due to original still C# has no such stuff)
    Added classes:
    Tree<T>
    TreeNode<T>

---
+++ TreeLib/Tree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeLib
+{
+    public class Tree<T> : IEnumerable<TreeNode<T>>
+    {
+        private TreeNode<T> _root;
+
+        public TreeNode<T> Root => _root;
+
+        private HashSet<TreeNode<T>> _nodesHeap = new HashSet<TreeNode<T>>();
+
+        public Tree(T rootValue)
+        {
+            _root = new TreeNode<T>(rootValue, null);
+            _nodesHeap.Add(_root);
+        }
+
+        public TreeNode<T> Add(T value, TreeNode<T> ancestor)
+        {
+            if (!_nodesHeap.Contains(ancestor))
+            {
+                throw new InvalidOperationException();
+            }
+            var newNode = new TreeNode<T>(value, ancestor);
+            ancestor.descendants.Add(newNode);
+            _nodesHeap.Add(newNode);
+            return newNode;
+        }
+
+        public void Remove(TreeNode<T> node)
+        {
+            if (!_nodesHeap.Contains(node))
+            {
+                throw new InvalidOperationException();
+            }
+            var parent = node.Ancestor;
+            parent.descendants.Remove(node);
+            _nodesHeap.Remove(node);
+            removeDescendants(node);
+        }
+
+        private void removeDescendants(TreeNode<T> node)
+        {
+            if (node.HasDescendants)
+            {
+                foreach (var descendant in node.Descendants)
+                {
+                    _nodesHeap.Remove(descendant);
+                    removeDescendants(descendant);
+                }
+            }
+        }
+
+        public IEnumerator<TreeNode<T>> GetEnumerator() => _nodesHeap.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    }
+}
---
+++ TreeLib/TreeNode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeLib
+{
+    public class TreeNode<T>
+    {
+        public T Value { get; set; }
+
+        internal TreeNode<T> ancestor;
+
+        internal HashSet<TreeNode<T>> descendants = new HashSet<TreeNode<T>>();
+
+        public TreeNode<T> Ancestor => ancestor;
+
+        public HashSet<TreeNode<T>> Descendants => descendants;
+
+        public bool HasDescendants => descendants != null && descendants.Count() != 0;
+
+        public TreeNode(TreeNode<T> ancestor)
+        {
+            this.ancestor = ancestor;
+        }
+
+        public TreeNode(T value, TreeNode<T> ancestor) : this(ancestor)
+        {
+            Value = value;
+        }
+
+        internal IEnumerable<TreeNode<T>> GetLeafs()
+        {
+            List<TreeNode<T>> result = new List<TreeNode<T>>();
+            foreach (var descendant in Descendants)
+            {
+                if (!descendant.HasDescendants)
+                {
+                    result.Add(descendant);
+                }
+                else
+                {
+                    result.AddRange(descendant.GetLeafs());
+                }
+            }
+            return result;
+        }
+    }
+}
